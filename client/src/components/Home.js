6ac9f0726caba71f00cda4f0beb75ba663e2bf71
Cris Cancino <crisnoel12@gmail.com>
2017-10-14 14:07:47 -0700

    updated jumbotron heading class to display-4

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     render() {
         return (
             <div className="jumbotron">
-                <h2 className="display-3 component-heading">Let's Get Productive!</h2>
+                <h2 className="display-4 component-heading">Let's Get Productive!</h2>
                 <p className="lead">A simple todo web application built with react.</p>
                 <hr className="my-4"/>
                 <Link to="/todos" className="btn btn-primary">Manage Todos</Link>
